aba1f3301947a439bfbbb6bd925932210697715a
p-fury <plutocamera@gmail.com>
2024-05-14 23:13:01 +0200

    pytests

--- Hero/asgi.py
+++ Hero/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Hero project.
+ASGI config for HeroProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hero.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HeroProject.settings')
 
 application = get_asgi_application()
--- Hero_app/tests.py
+++ Hero_app/tests.py
@@ -1,3 +1,29 @@
+import pytest
+
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_main_get():
+    client = Client()
+    url = reverse('main')
+    response = client.get(url)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_char_create_get():
+    client = Client()
+    url = reverse('char-create')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_char_get():
+    client = Client()
+    url = reverse('char')
+    response = client.get(url)
+    assert response.status_code == 200
